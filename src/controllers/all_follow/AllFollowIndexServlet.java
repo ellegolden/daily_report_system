5cbda03c235b7adc9c875d24c501db9ecba9621a
kaoru.nakashima <ellegolden.1@gmail.com>
2019-08-25 22:06:59 +0900

    complete

---
+++ src/controllers/all_follow/AllFollowIndexServlet.java
@@ -0,0 +1,79 @@
+package controllers.all_follow;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.Report;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class AllFollowIndexServlet
+ */
+@WebServlet("/all_follow/index")
+public class AllFollowIndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AllFollowIndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //EntityManagerの起動
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+
+        //開くページ数を取得(デフォルトは1ページ目)
+        int page;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch (Exception e) {
+            page =1;
+        } //初回はcatchを拾って44行目の page = 1が入る
+
+        //最大件数と開始位置を指定してフォロワーの情報を取得
+        List<Report> flReports = em.createNamedQuery("getFollowsAllReports", Report.class) //取ってくるデータの型を第2引数に指定
+                .setParameter("follower", login_employee)
+                .setParameter("follow" ,em.find(Employee.class, Integer.parseInt(request.getParameter("unfollow_id"))))
+                .setFirstResult(15 * (page - 1)) //最初にデータを取る位置を指定
+                .setMaxResults(15) //setFirstResultから最大でどこまでデータを取るか指定
+                .getResultList(); // NamedQueryに合致したデータをすべて取得
+
+        //全件数を取得
+        long flReports_count = (long) em.createNamedQuery("getFollowsReportsCount", Long.class) //countは数字を持ってくるのでlongを指定
+                .setParameter("follower", login_employee)
+                .getSingleResult();     //count1件だけなのでgetSingleResultで結果を1件取得
+
+        System.out.println(flReports_count + "件ヒットしました");
+        em.close();
+
+        request.setAttribute("flReports", flReports);
+        request.setAttribute("flReports_count", flReports_count);
+        request.setAttribute("page", page);
+
+        // ログイン後のTOPページにフラッシュメッセージを表示する
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));  // セッションスコープからリクエストスコープに格納場所を移動
+            request.getSession().removeAttribute("flush");      //セッションスコープの中を空にする
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/follow/index.jsp");
+        rd.forward(request, response);
+    }
+
+}
